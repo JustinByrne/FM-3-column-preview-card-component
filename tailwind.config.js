7eca33818d10426da1463ed3eb95cfd858063510
Justin Byrne <theskaterjesus@gmail.com>
2021-07-27 21:16:04 +0100

    added colours and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: 'hsl(31, 77%, 52%)',
+        darkCyan: 'hsl(184, 100%, 22%)',
+        vdarkCyan: 'hsl(179, 100%, 13%)',
+        vlightGray: 'hsl(0, 0%, 95%)',
+      },
+      fontFamily: {
+        deca: 'Lexend Deca, sans-serif',
+        shoulders: 'Big Shoulders Display, cursive',
+      }
+    },
   },
   variants: {
     extend: {},
